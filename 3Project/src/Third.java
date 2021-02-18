2fae45fa9392c07ab0091c7c366cf2e5db44ca55
pc <pc@DESKTOP-PR50DSE>
2021-02-18 01:36:53 -0600

    Project3

---
+++ 3Project/src/Third.java
@@ -0,0 +1,11 @@
+
+public class Third {
+
+	public static void main (String [] args) {
+		int a=10;
+		float b= (float) 12.5;
+		String c=("Java Programming");
+		
+		System.out.println(a + ", " + b+ ", " +c);
+	}
+}
